f728f30c5027256044e8a20cbbc846fd552cff93
Elias Assaf <elyas51000@gmail.com>
2021-03-29 15:50:09 +0300

    with state

--- lab2/DUT/top.vhd
+++ lab2/DUT/top.vhd
@@ -12,24 +12,25 @@ end top;
 ------------------------------------------------------------------
 architecture arc_sys of top is
 	signal currentBound: std_logic_vector(n-1 downto 0) := (others => '0');
-	signal tempcountOut: std_logic_vector(n-1 downto 0) := (others => '0');
+	signal tempOut: std_logic_vector(n-1 downto 0) := (others => '0');
+	signal state: std_logic := '1';
 	constant zerovec: std_logic_vector(n-1 downto 0) := (others => '0');	
 begin
 	--------------------------------------------------------------
 	proc1 : process(clk,rst)
 	begin
-		
 		if(rst ='1') then
-			tempcountOut <= zerovec;
+			tempOut <= zerovec;
 		elsif(clk'EVENT and clk = '1') then
-			if(currentBound = zerovec) then
-				tempcountOut <= zerovec;
-			elsif(currentBound = tempcountOut) then
-				tempcountOut <= zerovec;
-			else
-				tempcountOut <= tempcountOut + '1';
-			end if;
 
+			if(tempOut = currentBound) then
+				tempOut <= zerovec;
+			elsif(tempOut = upperBound) then
+				tempOut <= zerovec;
+			else
+				tempOut <= tempOut + '1';
+			end if;
+			
 		end if;
 	end process;
 	--------------------------------------------------------------
@@ -38,15 +39,20 @@ begin
 		if(rst ='1') then
 			currentBound <= zerovec;
 		elsif(clk'EVENT and clk = '0') then
-			if(tempcountOut = upperBound) then
+
+			if(state = '1') then
+				state <= '0';
+			elsif(currentBound = upperBound and tempOut = upperBound) then
 				currentBound <= zerovec;
-			elsif(tempcountOut = zerovec) then
+				state <= '1';
+			elsif(tempOut = zerovec) then
 				currentBound <= currentBound + '1';
 			end if;
+
 		end if;
 	end process;
 	--------------------------------------
-	countOut <= tempcountOut;
+	countOut <= tempOut;
 	
 	
 
